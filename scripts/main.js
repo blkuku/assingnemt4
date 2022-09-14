40f852c58cc437bcf19b55f36b2e7aca21bc0fff
blkuku <uyenbui.061299@gmail.com>
2022-09-14 17:22:31 +0200

    dfsadfasdfas

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,13 @@
 var rotationSpeed = 0.01;
 var myOtherBox = document.getElementById('myOtherBox');
 
-function spin(){
+function spinL(){
 	myOtherBox.object3D.rotation.x += rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
 }
-
-setInterval(spin, 16); //equivalent to 60 fps
-
-function spin(){
-	myOtherBox.object3D.rotation.x += rotationSpeed + 15;
+function spinR(){
+	myOtherBox.object3D.rotation.x += -rotationSpeed;
 	console.log(myOtherBox.object3D.rotation.x);
 }
+setInterval(spinL, 16); //equivalent to 60 fps
+setInterval(spinR, 16); //equivalent to 60 fps
